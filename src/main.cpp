a635b88c3e1368a2dfdf233e432f3b75402f4b37
Zegnar0 <zegnarkow@gmail.com>
2024-01-24 15:43:30 +0100

    Boids Context created

--- src/main.cpp
+++ src/main.cpp
@@ -11,25 +11,24 @@ int main()
 
     // Actual application code
     auto ctx = p6::Context{{.title = "Simple-p6-Setup"}};
-    auto        square_radius = 0.5f;
+    const p6::Angle angle  = 0.5_turn;
+    
     std::string text          = "Hello";
     ctx.maximize_window();
 
     // Declare your infinite update loop.
     ctx.update = [&]() {
-        ImGui::Begin("Test");
-        ImGui::SliderFloat("Square size", &square_radius, 0.f, 1.f);
-        ImGui::InputText("Text", &text);
-        ImGui::End();
+        // ImGui::Begin("Test");
+        // ImGui::SliderFloat("Square size", &square_radius, 0.f, 1.f);
+        // ImGui::InputText("Text", &text);
+        // ImGui::End();
         // Show the official ImGui demo window
         // It is very useful to discover all the widgets available in ImGui
-        ImGui::ShowDemoWindow();
+        // ImGui::ShowDemoWindow();
         ctx.background(p6::NamedColor::Blue);
-        ctx.circle(
-            p6::Center{ctx.mouse()},
-            p6::Radius{0.2f}
-        );
-        ctx.square(p6::Center{}, p6::Radius{square_radius});
+        ctx.fill = {1, 0, 0, 0.5};
+        ctx.equilateral_triangle(p6::Center{ctx.mouse()}, 0.1f, angle);
+
     };
 
     // Should be done last. It starts the infinite loop.
