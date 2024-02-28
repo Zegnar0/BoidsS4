d877f58aea2f62685eb99d79169f172d3c99a97b
[Lawayn] <mailis.bonhommr@gmail.com>
2024-02-28 17:44:25 +0100

    Mouvement Boids

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <cstdlib>
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest/doctest.h"
@@ -10,26 +11,59 @@ int main()
         return EXIT_FAILURE;
 
     // Actual application code
-    auto        ctx           = p6::Context{{.title = "Simple-p6-Setup"}};
-    auto        square_radius = 0.5f;
-    std::string text          = "Hello";
+    auto        ctx             = p6::Context{{.title = "Simple-p6-Setup"}};
+    auto        triangle_radius = 0.5f;
+    float       positionX       = 0.f;
+    float       positionY       = 0.f;
+    float       vitesseX        = p6::random::number(-2.f, 2.f);
+    float       vitesseY        = p6::random::number(-2.f, 2.f);
+    int         nb_triangle     = 5;
+    p6::Angle   angle           = 1_turn;
+    std::string text            = "Hello";
     ctx.maximize_window();
 
     // Declare your infinite update loop.
     ctx.update = [&]() {
-        ImGui::Begin("Test");
-        ImGui::SliderFloat("Square size", &square_radius, 0.f, 1.f);
-        ImGui::InputText("Text", &text);
+        ImGui::Begin("Param");
+        ImGui::SliderFloat("Triangle size", &triangle_radius, 0.f, 1.f);
+        ImGui::SliderFloat("Triangle vitesse X", &vitesseX, -2.f, 2.f);
+        ImGui::SliderFloat("Triangle vitesse Y", &vitesseY, -2.f, 2.f);
         ImGui::End();
-        // Show the official ImGui demo window
-        // It is very useful to discover all the widgets available in ImGui
-        ImGui::ShowDemoWindow();
         ctx.background(p6::NamedColor::Blue);
-        ctx.circle(
-            p6::Center{ctx.mouse()},
-            p6::Radius{0.2f}
-        );
-        ctx.square(p6::Center{}, p6::Radius{square_radius});
+        for (int i = 0; i < nb_triangle; i++)
+        {
+            if (round((ctx.time() - ctx.delta_time()) / 2) != round(ctx.time() / 2))
+            {
+                angle = p6::random::angle();
+            }
+            const auto sg1 = ctx.transform_scope_guard();
+            vitesseX       = std::cos(angle.as_radians());
+            vitesseY       = std::sin(angle.as_radians());
+            positionX += vitesseX * ctx.delta_time();
+            positionY += vitesseY * ctx.delta_time();
+            // position += vitesse * p6::Rotation(angle).value;
+            ctx.translate({positionX, positionY});
+            {
+                if (positionX > ctx.aspect_ratio())
+                {
+                    positionX = -ctx.aspect_ratio();
+                }
+                if (positionX < -ctx.aspect_ratio())
+                {
+                    positionX = ctx.aspect_ratio();
+                }
+                if (positionY > 1)
+                {
+                    positionY = -1;
+                }
+                if (positionY < -1)
+                {
+                    positionY = 1;
+                }
+                ctx.fill = {1, 0, 0, 0.5};
+                ctx.equilateral_triangle(p6::Center{}, triangle_radius, angle);
+            }
+        }
     };
 
     // Should be done last. It starts the infinite loop.
